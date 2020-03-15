7473bfb0885827a90f5acb2213cc15a88a4aee87
Christelle Canovas <christelle.canovas@gmail.com>
2020-03-15 20:34:09 +0100

    First unit test on Entity User $username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,4 +7,16 @@ namespace App\Entity;
 class User
 {
 
+    private $username;
+
+    public function __construct(string $username)
+    {
+        $this->username = $username;
+    }
+
+    public function getUserName()
+    {
+        return $this->username;
+    }
+
 }
--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -3,10 +3,19 @@
 
 namespace App\Tests\Entity;
 
-
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+
+    public function testUserNameExists(){
+
+        $user = new User('user1');
+        $this -> assertSame('user1',$user->getUserName());
+
+
+
+    }
 
 }
