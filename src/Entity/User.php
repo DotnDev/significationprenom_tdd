c84492eeedbde3ff772313222955bde2c151e92f
Christelle Canovas <christelle.canovas@gmail.com>
2020-03-15 21:07:28 +0100

    More unit tests on User Entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,15 +8,45 @@ class User
 {
 
     private $username;
+    private $firstname;
+    private $lastname;
+    private $firstpassword;
+    private $secondpassword;
 
-    public function __construct(string $username)
+
+    public function __construct(string $username, string $firstname, string $lastname, string $firstpassword, string $secondpassword)
     {
         $this->username = $username;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+        $this->firstpassword = $firstpassword;
+        $this->secondpassword = $secondpassword;
+
     }
 
     public function getUserName()
     {
         return $this->username;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstname;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastname;
+    }
+
+    public function getFirstPassword()
+    {
+        return $this->firstpassword;
+    }
+
+    public function getSecondPassword()
+    {
+        return $this->secondpassword;
     }
 
 }
--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -9,13 +9,39 @@ use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase
 {
 
-    public function testUserNameExists(){
 
-        $user = new User('user1');
-        $this -> assertSame('user1',$user->getUserName());
+    public function testUserNameExists()
+    {
+        $user = new User('user1','Bill','Cosby','password','password');
+        $this -> assertNotEmpty($user->getUserName(),$error="username is empty");
+    }
 
+    public function testFirstNameExists()
+    {
+        $user = new User('user1','Bill','Cosby','password','password');
+        $this -> assertNotEmpty($user->getFirstName(),$error="firstname is empty");
+    }
 
+    public function testLastNameExists()
+    {
+        $user = new User('user1','Bill','Cosby','password','password');
+        $this -> assertNotEmpty($user->getLastName(),$error="lastname is empty");
+    }
+
+    public function testFirstPasswordExists()
+    {
+        $user = new User('user1','Bill','Cosby','password','password');
+        $this -> assertNotEmpty($user->getFirstPassword(),$error="paswword is empty");
 
     }
+
+    public function testSecondPasswordExists()
+    {
+        $user = new User('user1','Bill','Cosby','password','password');
+        $this -> assertNotEmpty($user->getFirstPassword(),$error="paswword is empty");
+
+    }
+
+
 
 }
